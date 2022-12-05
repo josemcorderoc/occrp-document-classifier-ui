75d87635af0eb480ba5f61046ba23388f3449819
José Miguel Cordero Carvaco <josemcorderoc@gmail.com>
2022-12-05 14:07:16 +0000

    docs: remove useless comments

--- frontend/js/predict_file.js
+++ frontend/js/predict_file.js
@@ -6,23 +6,17 @@ async function predictFile() {
     let data = new FormData();
     data.append('uploaded_file', document.querySelector("#formFile").files[0]);
 
-    // send fetch along with cookies
     let response = await fetch('http://localhost:8000/predict', {
         method: 'POST',
         credentials: 'same-origin',
         body: data
     });
 
-    // server responded with http response != 200
     if(response.status != 200)
         throw new Error('HTTP response code != 200');
 
-    // read json response from server
-    // success response example : {"error":0,"message":""}
-    // error response example : {"error":1,"message":"File type not allowed"}
     let json_response = await response.json();
     if(json_response.error == 1)
         throw new Error(json_response.message);
     console.log(json_response["prediction"])
-	// return json_response;
 }
