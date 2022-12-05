26eb020b634d5f0aa85c5162245878bce2f7094e
José Miguel Cordero Carvaco <josemcorderoc@gmail.com>
2022-12-05 19:17:54 +0000

    feat: render predicted results

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -1,144 +1,65 @@
-/*!
-* Start Bootstrap - Landing Page v6.0.5 (https://startbootstrap.com/theme/landing-page)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-landing-page/blob/master/LICENSE)
-*/
+document.querySelector("#formFile").addEventListener('change', predictFile);
 
-$(document).ready(function(){
-    
-    let statsJSONInput = 
-    `{
-        "page-1": [
-          {
-            "class-1": "0.9",
-            "class-2": "0.1",
-            "class-3": "0.2",
-            "class-4": "0.0",
-            "class-5": "0.3",
-            "class-6": "0.2",
-            "class-7": "0.1",
-            "class-8": "0.0",
-            "class-9": "0.2"
-          }
-        ],
-        "page-2": [
-          {
-            "class-1": "0.3",
-            "class-2": "0.2",
-            "class-3": "0.1",
-            "class-4": "0.0",
-            "class-5": "0.5",
-            "class-6": "0.7",
-            "class-7": "0.5",
-            "class-8": "0.2",
-            "class-9": "0.1"
-          }
-        ],
-        "page-3": [
-          {
-            "class-1": "0.2",
-            "class-2": "0.3",
-            "class-3": "0.2",
-            "class-4": "0.1",
-            "class-5": "0.4",
-            "class-6": "0.6",
-            "class-7": "0.9",
-            "class-8": "0.2",
-            "class-9": "0.3"
-          }
-        ]
-      }`
-    
-    const stats = JSON.parse(statsJSONInput);
+async function predictFile() {
+    console.log("Starting prediction...")
 
+    let data = new FormData();
+    data.append('uploaded_file', document.querySelector("#formFile").files[0]);
 
-    $("#showbutton").click(function(){
-
-        
-        for (const [pagekey, pagevalue] of Object.entries(stats)) {
-            
-            let clone = $("#results-template").clone()
-
-            // clone.removeAttr('id');
-            clone.show();
-            
-            // set ids
-            clone.attr('id', pagekey)
-            $(".results-graph-pseudo", clone).attr('id', pagekey + '-graph')
-            
-            $(".order-lg-1 > h2", clone).text(pagekey);
-
-            clone.appendTo("#result-section");
-
-            let someresults = "";
-            var someresultsArray = ["0.3", "0.7"];
-            for (const [classkey, classvalue] of Object.entries(pagevalue[0])) {
-                
-                someresults = someresults + " " + classvalue;
-                someresultsArray.push(parseFloat(classvalue));
-            }
-
-            // $(".order-lg-2 > .results-graph-pseudo", clone).text(someresults);
-
-
-            var data = [
-              {
-              x: [
-                  'bank statement',
-                  'company registry',
-                  'contract',
-                  'court document',
-                  'gazette',
-                  'invoice',
-                  'passport',
-                  'receipt',
-                  'other'
-              ],
-              y: someresultsArray,
-              type: 'bar'
-              }
-            ];
-        
-            Plotly.newPlot(pagekey + '-graph', data);
-            
-            // prediction
-            //$(".order-lg-2 > .results-prediction-pseudo", clone).text("Bank statement");
-
-        }
-  
-
+    let response = await fetch('http://localhost:8000/predict', {
+        method: 'POST',
+        credentials: 'same-origin',
+        body: data
     });
 
+    if(response.status != 200)
+        throw new Error('HTTP response code != 200');
 
-    $("#hidebutton").click(function(){
+    let json_response = await response.json();
+    if(json_response.error == 1)
+        throw new Error(json_response.message);
+    console.log(json_response)
+    renderResults(json_response)
+}
 
-        
-      for (const [pagekey, pagevalue] of Object.entries(stats)) {
-          
-          let clone = $("#results-template").clone()
+function renderResults(response) {
+  cleanResults()
 
-          clone.removeAttr('id');
-          clone.show();
-          
-
-          $(".order-lg-1 > h2", clone).text(pagekey);
-
-          clone.appendTo("#result-section");
-
-          let someresults = "";
-          for (const [classkey, classvalue] of Object.entries(pagevalue[0])) {
-              
-              someresults = someresults + " " + classvalue;
-          }
-          
-          console.log(someresults);
-
-          $(".order-lg-2 > .results-pseudo-class", clone).text(someresults);
-
-      }
+  let predictions = response["prediction"]
+  for(let i = 0; i < predictions.length; i++) {
+    let prediction = predictions[i];
 
 
-  });
+    let predicted_label = prediction["__predicted"]
+    delete prediction["__predicted"]
 
-});
+    let page = "Page " + (i+1)
+    let name = "page" + i
+    let graph_name = name + '-graph'
 
+    let clone = $("#results-template").clone()
+
+    clone.show();    
+    clone.attr('id', name)
+
+    $(".results-graph-pseudo", clone).attr('id', graph_name)
+    $(".results-prediction-pseudo", clone).text(predicted_label)
+
+    $(".order-lg-1 > h2", clone).text(page);
+
+    clone.appendTo("#result-section");
+
+    let data = [{
+      x: Object.keys(prediction),
+      y: Object.values(prediction),
+      type: 'bar'
+    }];
+
+    Plotly.newPlot(graph_name, data);
+  }
+}
+
+function cleanResults() {
+  const results = document.getElementById("result-section");
+  results.innerHTML = '';
+}
